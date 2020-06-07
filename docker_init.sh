da792b5ce566de074ac0b2530eadad21d9bbb576
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2020-06-07 09:18:48 -0400

    Add Dockerfiles to package project into docker images

---
+++ docker_init.sh
@@ -0,0 +1,2 @@
+docker build . -f Dockerfile.base_img -t htne/base_img
+docker build . -f Dockerfile.instance -t htne/htne_instance
---
+++ run.sh
@@ -0,0 +1,8 @@
+/etc/init.d/postgresql start
+cd /project/
+ls
+# npm install
+npm start
+echo "Finished."
+read
+echo "Closing application"
